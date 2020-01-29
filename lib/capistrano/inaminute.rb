42119c47e9dbf15df4bd814240ee45da53d94943
grezar <grezar.dev@gmail.com>
2020-01-29 21:13:24 +0900

    exec: bundle gem capistrano-inaminute

---
+++ lib/capistrano/inaminute.rb
@@ -0,0 +1,8 @@
+require "capistrano/inaminute/version"
+
+module Capistrano
+  module Inaminute
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/inaminute/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Inaminute
+    VERSION = "0.1.0"
+  end
+end
