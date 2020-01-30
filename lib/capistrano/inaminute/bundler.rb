2b88ab7844c45082c39e46693ecc18984f59dd08
grezar <grezar.dev@gmail.com>
2020-01-30 22:49:32 +0900

    Fix bundler within path

--- lib/capistrano/inaminute/bundler.rb
+++ lib/capistrano/inaminute/bundler.rb
@@ -2,7 +2,7 @@ require 'capistrano/inaminute/base'
 
 class Capistrano::Inaminute::Bundler < Capistrano::Inaminute::Base
   def install
-    within release_path do
+    within fetch(:inaminute_local_release_path) do
       execute :bundle, "install", fetch(:inaminute_bundle_install_opts)
     end
   end
