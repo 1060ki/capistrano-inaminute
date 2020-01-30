8c2e0f6a2ace28d542deb61bb3fd0ebff630ed59
grezar <grezar.dev@gmail.com>
2020-01-30 23:16:31 +0900

    Fix rails tasks to run within local release path and with clean env

--- lib/capistrano/inaminute/rails.rb
+++ lib/capistrano/inaminute/rails.rb
@@ -2,25 +2,31 @@ require 'capistrano/inaminute/base'
 
 class Capistrano::Inaminute::Rails < Capistrano::Inaminute::Base
   def assets_precompile
-    within release_path do
-      execute :rake, "assets:precompile"
+    within fetch(:inaminute_local_release_path) do
+      Bundler.with_clean_env do
+        execute :bundle, "exec", "rake", "assets:precompile"
+      end
     end
   end
 
   def db_migrate
-    within release_path do
+    within fetch(:inaminute_local_release_path) do
       with rails_env: fetch(:rails_env) do
-        execute :rake, "db:create"
-        execute :rake, "db:migrate"
+        Bundler.with_clean_env do
+          execute :bundle, "exec", "rake", "db:create"
+          execute :bundle, "exec", "rake", "db:migrate"
+        end
       end
     end
   end
 end
 
 def db_seed
-  within release_path do
+  within fetch(:inaminute_local_release_path) do
     with rails_env: fetch(:rails_env) do
-      execute :rake, "db:seed"
+      Bundler.with_clean_env do
+        execute :bundle, "exec", "rake", "db:seed"
+      end
     end
   end
 end
