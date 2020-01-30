ce80222c0f96709ce13d2c08ce83adddecddf201
grezar <grezar.dev@gmail.com>
2020-01-30 22:02:35 +0900

    Sepcify branch on clone

--- lib/capistrano/inaminute/git.rb
+++ lib/capistrano/inaminute/git.rb
@@ -7,7 +7,7 @@ class Capistrano::Inaminute::Git < Capistrano::Inaminute::Base
 
   def clone
     within fetch(:inaminute_local_release_path) do
-      execute :git, "clone", fetch(:repo_url), fetch(:inaminute_local_release_path)
+      execute :git, "clone", fetch(:repo_url), fetch(:inaminute_local_release_path), "-b", "#{fetch(:branch)}"
       execute :git, "reset", "--hard", "origin/#{fetch(:branch)}"
     end
   end
