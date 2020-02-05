afc26cfd9d62d9bf6b2992b37c20e6f6d662dfee
grezar <grezar.dev@gmail.com>
2020-02-05 16:47:38 +0900

    Run bundle install without clean env

--- lib/capistrano/inaminute/bundler.rb
+++ lib/capistrano/inaminute/bundler.rb
@@ -3,9 +3,7 @@ require 'capistrano/inaminute/base'
 class Capistrano::Inaminute::Bundler < Capistrano::Inaminute::Base
   def install
     within fetch(:inaminute_local_release_path) do
-      Bundler.with_clean_env do
-        execute :bundle, "install", fetch(:inaminute_bundle_install_opts)
-      end
+      execute :bundle, "install", fetch(:inaminute_bundle_install_opts)
     end
   end
 end
